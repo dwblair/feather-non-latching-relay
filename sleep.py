a5cfe4fe6aed8603e8c4540cdf0fea1cac9f56ba
dwblair <donblair@gmail.com>
2016-09-04 23:43:42 -0400

    update

--- sleep.py
+++ sleep.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser(description='Read from serial and echo message'
 parser.add_argument('-d', '--device', help='path to serial device', required=True)
 parser.add_argument('-r', '--rate', help='rate of serial communication', type=int, required=True)
 parser.add_argument('-w', '--wait', help='seconds to wait before shutdown', type=int, required=True)
-parser.add_argument('-s', '--sleep', help='seconds to sleep', type=int, required=True)
+parser.add_argument('-s', '--sleep', help='turn off relay, then wait this many seconds, then turn relay back on', type=int, required=True)
 args = parser.parse_args()
 
 DEVICE = args.device
@@ -13,7 +13,7 @@ RATE = args.rate
 WAIT = args.wait
 SLEEP = args.sleep
 
-print 'Waiting for %d seconds, then shutting down for %d seconds ...' % (WAIT, SLEEP)
+print 'Waiting for %d seconds, then turning relay off for %d seconds ...' % (WAIT, SLEEP)
 
 ser = serial.Serial(DEVICE, RATE)
 
